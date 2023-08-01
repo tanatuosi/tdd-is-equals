8dc8d519ba50028afe6bd92176e51f6ce7ac6c38
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2023-08-01 15:06:18 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEquals(a: any, b: any): boolean {
-  return a === b;
+  const typeA = Object.prototype.toString.call(a);
+  const typeB = Object.prototype.toString.call(b);
+  if (typeA != typeB) {
+    return false;
+  }
+  if (typeA == "[object Number]" && typeB == "[object Number]" && isNaN(a) && isNaN(b)) {
+    return true; 
+  }
+  if (isBasicType(typeA) && isBasicType(typeB)) return a == b;
+  if ((typeA == "[object Object]" && typeB == "[object Object]") || (typeA == "[object Array]" || typeA == "[object Array]")){
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+    if(keysA.length != keysB .length) return false;
+    for( let key of keysA) {
+      if(!keysB.includes(key)) return false;
+      if(!isEquals(a[key],b[key])) return false;
+    }
+    return true;
+  }
+  return false;
 }
+
+const isBasicType = (type: string) => {
+  if (
+    type == "[object Number]" ||
+    type == "[object String]" ||
+    type == "[object Boolean]" ||
+    type == "[object Undefined]" ||
+    type == "[object Null]" ||
+    type == "[object Symbol]"
+  )
+    return true;
+  return false;
+};
